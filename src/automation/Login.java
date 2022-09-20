9e41117a98cd1f8ec057764b9c1585cd7f4d2411
mansur <mansurk184@gmail.com>
2022-09-20 12:11:57 +0545

    automation testing

---
+++ src/automation/Login.java
@@ -0,0 +1,36 @@
+package automation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	public static void main(String[] args){
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Mansur\\Downloads\\chromedriver_win32\\chromedriver.exe");	
+	  WebDriver driver=new ChromeDriver();
+	  String URL="http://automationpractice.com/index.php";
+	  
+	  // Open URL and Maximize browser window
+	  driver.get(URL);
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+	  
+	  //Click on Sign in
+	  driver.findElement(By.linkText("Sign in")).click();
+	  //Login
+	  driver.findElement(By.id("email")).sendKeys("test1249@test.com");
+	  driver.findElement(By.id("passwd")).sendKeys("PKR@PKR");
+	  driver.findElement(By.id("SubmitLogin")).click();
+	  String userText=driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).getText();
+	  
+	  //succesfull login
+	  if(userText.contains("Vsoft")) {
+		   System.out.println("Login sucessfull");
+		  }
+		  else {
+		   System.out.println("Login Failed");
+		  }
+	}
+}
---
+++ src/automation/SearchProduct.java
@@ -0,0 +1,45 @@
+package automation;
+import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+public class SearchProduct {
+	 public static void main(String[] args) throws InterruptedException{
+
+		 System.setProperty("webdriver.chrome.driver","C:\\Users\\Mansur\\Downloads\\chromedriver_win32\\chromedriver.exe");	
+		  WebDriver driver=new ChromeDriver();
+		  String URL="http://automationpractice.com/index.php";
+
+		  driver.get(URL);
+		  driver.manage().window().maximize();
+		  
+		  // Initialise Actions class object
+		  Actions actions=new Actions(driver);
+		  driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+		  WebElement womenTab=driver.findElement(By.linkText("WOMEN"));
+		  WebElement TshirtTab=driver.findElement(By.xpath("//div[@id='block_top_menu']/ul/li[1]/ul/li[1]/ul//a[@title='T-shirts']"));
+		  actions.moveToElement(womenTab).moveToElement(TshirtTab).click().perform();
+		  Thread.sleep(2000);
+		  
+		  // Get Product Name
+		  String ProductName=driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[2]/ul[1]/li[1]/div[1]/div[2]/h5[1]/a[1]")).getText();
+		  System.out.println(ProductName);
+		  driver.findElement(By.id("search_query_top")).sendKeys(ProductName);
+		  driver.findElement(By.name("submit_search")).click();
+		  
+		  // Get Name of Searched Product
+		  String SearchResultProductname=driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[2]/ul[1]/li[1]/div[1]/div[2]/h5[1]/a[1]")).getText();
+
+		  // Verify that correct Product is displaying after search
+		  if(ProductName.equalsIgnoreCase(SearchResultProductname)) {
+		   System.out.println("Results Matched;Test Case Passed");
+		  }else{
+		   System.out.println("Results NotMatched;Test Case Failed");
+		  }
+		  
+		  // Close the browser
+		  driver.close();
+		 }
+}
